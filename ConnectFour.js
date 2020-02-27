f91e9e50592f49df96faedf87143973c06b95811
Jared <jlander168@learner.dvusd.org>
2020-02-27 14:46:30 -0700

    Javascript for the table and tracking player
    Fixed and finished the winning function for the game.
    Added in a working scoreboard.
    Added in a tracker for which player is playing and who wins.
    Made the new game button work.

--- ConnectFour.js
+++ ConnectFour.js
@@ -1,7 +1,9 @@
 var game_active = false; 
 var active_player = 0;
 var gameboard = [];
-var player_color = []; 
+var player_color = [];
+var player1_score = 0;
+var player2_score = 0;
 player_color[1] = "red"; 
 player_color[2] = "yellow"; 
 
@@ -14,30 +16,31 @@ for (row=0; row<=5; row++) {
 
 function startGame() {
     if(game_active == true) return false;
-    game_active=true
-    active_player = 1; 
+    game_active=true;
+
+    //If there are any pieces on the board, they are cleared
+    for (row=0; row<=5; row++) {
+        gameboard[row] = [];
+        for (col=0; col<=6; col++) {
+            gameboard[row][col] = 0;
+        }	
+    }
+
+    active_player = Math.floor(Math.random()*2)+1;
+    showTurn();
     drawBoard();
-    
 }
 function drawBoard() {
-    
-    if(game_active==false) return;
+    if(game_active == false) return
     checkForWin();
     for (col = 0; col<=6; col++) {
         for (row=0; row<=5; row++) {
             document.getElementById('square_'+row+'_'+col).outerHTML ="<td id='square_" + row + "_"+ col +"' class='board_square player"+gameboard[row][col]+"' onclick='turn("+col+")' onhover='hover("+col+")'></td>";
         }	
     }
+
 }
 function checkForWin() {
-    /* There are many ways this algorithm can be accomplished.  Basically you want to check all possibility for a win.
-    Given the size of the board, checking all possibilities will not be a huge task for the computer, so I will go 
-    with an easy to understand, straightforward algorithm */
-    /* Ultimately there are 4 ways to win - left-to-right, diagnol up, diagnol down, and top to bottom.  This 
-    function will check each of these situations twice - one for each player. */
-    
-    //check left-to-right
-    //check for player 1 and 2
     for (i=1; i<=2; i++) {
         //since a winning row must be 4 long, we only need to check the first 3 rows, 0,1,and 2
         for (col = 0; col <=3; col++) {
@@ -61,7 +64,6 @@ function checkForWin() {
                 //check to see if the gameboard item is set to the player we are checking, if so, lets check the next 3 for a match
                 if (gameboard[row][col] == i) {
                     if ((gameboard[row+1][col] == i) && (gameboard[row+2][col] == i) && (gameboard[row+3][col] == i)) {
-                        drawBoard();
                         endGame(i); //a match has been made - run endGame for the player who had the match.
                         return true; //stop checking for a win - the game is over.
                     }
@@ -79,7 +81,6 @@ function checkForWin() {
                 //check to see if the gameboard item is set to the player we are checking, if so, lets check the next 3 for a match
                 if (gameboard[row][col] == i) {
                     if ((gameboard[row+1][col+1] == i) && (gameboard[row+2][col+2] == i) && (gameboard[row+3][col+3] == i)) {
-                        drawBoard();
                         endGame(i);
                         return true;
                     }
@@ -97,7 +98,6 @@ function checkForWin() {
                 //check to see if the gameboard item is set to the player we are checking, if so, lets check the next 3 for a match
                 if (gameboard[row][col] == i) {
                     if ((gameboard[row-1][col+1] == i) && (gameboard[row-2][col+2] == i) && (gameboard[row-3][col+3] == i)) {
-                        drawBoard();
                         endGame(i);
                         return true;
                     }
@@ -106,8 +106,22 @@ function checkForWin() {
         }
     }
 }
-function endGame() {
+function endGame(winner) {
     game_active = false; //set the "game_active" to false, so that it can be started again.
+    document.getElementById("player").innerHTML = "Player"+winner+" wins";
+    if(winner==1) {
+        player1_score++;
+        document.getElementById("redPlayer").innerHTML = player1_score;
+    } else if(winner==2) {
+        player2_score++;
+        document.getElementById("yellowPlayer").innerHTML = player2_score;
+    }
+}
+
+function showTurn() {
+    if(game_active==true) {
+        document.getElementById("player").innerHTML = "Now Player: Player"+[active_player];
+    }
 }
 
 function turn(col) {
@@ -120,6 +134,7 @@ function turn(col) {
             } else if(active_player == 2) {
                 active_player = 1;
             }
+            showTurn();
             return true
         }
     }
